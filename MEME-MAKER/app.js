42282fe590af78dcba46cb38c7af7c51b78b1970
Namyunha <108773845+Namyunha@users.noreply.github.com>
2022-10-19 20:29:48 +0900

    Paint Color part Two

--- MEME-MAKER/app.js
+++ MEME-MAKER/app.js
@@ -1,6 +1,7 @@
 const lineWidth = document.querySelector("#line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const color = document.querySelector("#color");
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
@@ -26,9 +27,15 @@ function onChangeWidth(event) {
   ctx.lineWidth = event.target.value;
 }
 
+function onChange(event) {
+  ctx.fillStyle = event.target.value;
+  ctx.strokeStyle = event.target.value;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", canclePainting);
 canvas.addEventListener("mouseleave", canclePainting);
 
 lineWidth.addEventListener("change", onChangeWidth);
+color.addEventListener("change", onChange);
