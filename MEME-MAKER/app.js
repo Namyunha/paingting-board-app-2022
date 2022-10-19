c7068294b0c0b181704243f916e6456c699aba5c
Namyunha <108773845+Namyunha@users.noreply.github.com>
2022-10-19 22:07:07 +0900

    Filling Mode

--- MEME-MAKER/app.js
+++ MEME-MAKER/app.js
@@ -1,3 +1,4 @@
+const modeBtn = document.getElementById("mode-btn");
 const colorOptions = Array.from(
   document.getElementsByClassName("color-option")
 );
@@ -9,6 +10,7 @@ canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
 let isPainting = false;
+let isFilling = false;
 
 function onMove(event) {
   if (isPainting) {
@@ -40,14 +42,31 @@ function onColorClick(event) {
   ctx.fillStyle = colorValues;
   color.value = colorValues;
 }
+function onModeClick() {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw";
+  }
+}
+
+function onCanvasClick() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, 800, 800);
+  }
+}
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", canclePainting);
 canvas.addEventListener("mouseleave", canclePainting);
-
+canvas.addEventListener("click", onCanvasClick);
 lineWidth.addEventListener("change", onChangeWidth);
 color.addEventListener("change", onChange);
 
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
 console.log(colorOptions);
+
+modeBtn.addEventListener("click", onModeClick);
